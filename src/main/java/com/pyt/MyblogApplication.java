b71d52368aea27e5a714acc86c6bd0f96b0a15c4
your name <you@example.com>
2020-04-02 15:12:14 +0800

    提交变换2

--- src/main/java/com/pyt/MyblogApplication.java
+++ src/main/java/com/pyt/MyblogApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyblogApplication {
 
 	public static void main(String[] args) {
-		System.out.println("123");
+
 		SpringApplication.run(MyblogApplication.class, args);
 	}
 
