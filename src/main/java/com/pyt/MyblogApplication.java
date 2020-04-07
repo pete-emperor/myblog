0c0428e8903a5b7ba2f4fdfb58baec2d00cdd15b
your name <you@example.com>
2020-04-07 17:26:06 +0800

    wed

--- src/main/java/com/pyt/MyblogApplication.java
+++ src/main/java/com/pyt/MyblogApplication.java
@@ -1,8 +1,14 @@
 package com.pyt;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
+@EnableAutoConfiguration
+@ComponentScan(value = "com.pyt")
+@PropertySource("application.properties")
 @SpringBootApplication
 public class MyblogApplication {
 
