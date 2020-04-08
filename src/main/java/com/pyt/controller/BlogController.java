90dcfd09d7ed16364b4bd53a8df372b43b244720
your name <you@example.com>
2020-04-08 16:50:11 +0800

    BlogController

--- src/main/java/com/pyt/controller/BlogController.java
+++ src/main/java/com/pyt/controller/BlogController.java
@@ -6,12 +6,14 @@ import com.pyt.bean.UserInfo;
 import com.pyt.service.BlogService;
 import com.pyt.service.TaskService;
 import com.pyt.service.UserService;
+import com.pyt.util.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.ClassUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
@@ -26,15 +28,34 @@ public class BlogController {
     @Value("${page.size}")
     private Integer pageSize;
 
+    @Resource
+    private RedisUtil redisUtil;
+
     @Autowired
     private BlogService blogService;
 
     @RequestMapping("/getBlogList")
     public List<Blog> getBlogList(HttpServletRequest request){
         Blog blog = new Blog();
-        blog.setPageIndex(Integer.valueOf(request.getParameter("pageIndex")));
-        blog.setPageSize(Integer.valueOf(request.getParameter("pageSize")));
-        return  blogService.getBlogList(blog);
+        List<Blog> list = null;
+        String pageIndex = request.getParameter("pageIndex");
+        String pageSize = request.getParameter("pageSize");
+
+        if(!redisUtil.hasKey(pageIndex+"_"+pageSize)){
+            blog.setPageIndex(Integer.valueOf(pageIndex));
+            blog.setPageSize(Integer.valueOf(pageSize));
+            list =  blogService.getBlogList(blog);
+            redisUtil.set(pageIndex+"_"+pageSize,list,30l);
+        }else{
+            list = (List<Blog>)redisUtil.get(pageIndex+"_"+pageSize);
+        }
+        if(redisUtil.hasKey("page")){
+            redisUtil.set("page",redisUtil.get("page")+","+pageIndex+"_"+pageSize);
+        }else{
+            redisUtil.set("page",pageIndex+"_"+pageSize);
+        }
+
+        return  list;
     }
 
     @RequestMapping("/getBlogPage")
