3bbdbc82e30ec2b2202b42fb0a5c57d83ea882c7
your name <1976479003@qq.com>
2020-04-20 18:13:44 +0800

    擦擦擦

--- src/main/java/com/pyt/scheduled/ArticleWebSpider.java
+++ src/main/java/com/pyt/scheduled/ArticleWebSpider.java
@@ -85,7 +85,8 @@ public class ArticleWebSpider implements ApplicationRunner {
 		Integer type = articleTask.getType();
 
 		List<String> firstUrlList = new ArrayList<String>();
-		firstUrlList = getPageUrl(firstUrlList,indexUrl,ignoreStr,firstUrlRegex0,firstUrlRegex1,firstUrlRegex2,firstUrlRegex3);
+		List<String> secondUrlList = new ArrayList<String>();
+		firstUrlList = getPageUrl(firstUrlList,secondUrlList,indexUrl,ignoreStr,firstUrlRegex,secondUrlRegex);
 
 
 
@@ -94,7 +95,16 @@ public class ArticleWebSpider implements ApplicationRunner {
 	}
 
 	//爬取分页url
-	public static List<String> getPageUrl(List <String> urlList,String indexUrl,String ignoreStr,String Regex0,String Regex1,String Regex2,String Regex3){
+	public static List<String> getPageUrl(List <String> firstUrlList,List<String> secondUrlList,String splitStr,String indexUrl,String ignoreStr,String firstUrlRegex,String secondUrlRegex){
+
+
+
+
+
+		String secondUrlRegex0 = secondUrlRegex.split(splitStr)[0];
+		String secondUrlRegex1 = secondUrlRegex.split(splitStr)[1];
+		String secondUrlRegex2 = secondUrlRegex.split(splitStr)[2];
+		String secondUrlRegex3 = secondUrlRegex.split(splitStr)[3];
 
 		URLConnection urlconn = null;
 		BufferedReader br = null;
@@ -102,6 +112,7 @@ public class ArticleWebSpider implements ApplicationRunner {
 		try {
 			url = new URL(indexUrl);
 			urlconn = url.openConnection();
+			urlconn.setConnectTimeout(10000);
 			br = new BufferedReader(new InputStreamReader(urlconn.getInputStream(), "UTF-8"));
 
 			StringBuffer sbPage = new StringBuffer();
@@ -109,33 +120,21 @@ public class ArticleWebSpider implements ApplicationRunner {
 			while((strTemp = br.readLine()) != null){
 				sbPage.append(strTemp);
 			}
+			br.close();
 
-			List<String> firstUrlList = new ArrayList<String>();
-			Pattern preFirstUrl = Pattern.compile(Regex0);
-			Matcher mreFirstUrl = preFirstUrl.matcher(sbPage.toString());
+
+
+			Pattern preSecondUrl = Pattern.compile(secondUrlRegex0);
+			Matcher mreSecondUrl = preSecondUrl.matcher(sbPage.toString());
+
+
+
 
 			while (mreFirstUrl.find()) {
-				String temp = mreFirstUrl.group(0);
-				if(Regex2.equals(ignoreStr)){
-					if(!Regex3.equals(ignoreStr)){
-						temp = temp.substring(0,temp.lastIndexOf(Regex3));
-					}
-				}else{
-					if(!Regex3.equals(ignoreStr)){
-						temp = temp.substring(temp.indexOf(Regex2) + Regex2.length(),temp.lastIndexOf(Regex3));
-					}else{
-						temp = temp.substring(temp.indexOf(Regex2) + Regex2.length(),temp.length());
-					}
-				}
-				String indexUrlTemp = "";
-				if(!Regex1.equals(ignoreStr)){
-					indexUrlTemp = Regex1 + temp;
-				}else{
-					indexUrlTemp = temp;
-				}
-				if(!urlList.contains(indexUrlTemp)){
-					urlList.add(indexUrlTemp);
-					getPageUrl(urlList,indexUrlTemp,ignoreStr,Regex0,Regex1,Regex2,Regex3);
+				String indexUrlTemp = getRegContent(ignoreStr,firstUrlRegex,);
+				if(!firstUrlList.contains(indexUrlTemp)){
+					firstUrlList.add(indexUrlTemp);
+					getPageUrl(firstUrlList,secondUrlList,indexUrlTemp,ignoreStr,Regex0,Regex1,Regex2,Regex3);
 				}
 			}
 
@@ -146,6 +145,37 @@ public class ArticleWebSpider implements ApplicationRunner {
 		}
 		return urlList;
 
+	}
+
+	public static String getRegContent(String ignoreStr,String urlRegex,String splitStr,String sbPage){
+
+		String urlRegex0 = urlRegex.split(splitStr)[0];
+		String urlRegex1 = urlRegex.split(splitStr)[1];
+		String urlRegex2 = urlRegex.split(splitStr)[2];
+		String urlRegex3 = urlRegex.split(splitStr)[3];
+
+		Pattern pre = Pattern.compile(urlRegex0);
+		Matcher mre = pre.matcher(sbPage.toString());
+		String temp = mre.group(0);
+		if(urlRegex2.equals(ignoreStr)){
+			if(!urlRegex3.equals(ignoreStr)){
+				temp = temp.substring(0,temp.lastIndexOf(urlRegex3));
+			}
+		}else{
+			if(!urlRegex3.equals(ignoreStr)){
+				temp = temp.substring(temp.indexOf(urlRegex2) + urlRegex2.length(),temp.lastIndexOf(urlRegex3));
+			}else{
+				temp = temp.substring(temp.indexOf(urlRegex2) + urlRegex2.length(),temp.length());
+			}
+		}
+		String urlTemp = "";
+		if(!urlRegex1.equals(ignoreStr)){
+			urlTemp = urlRegex1 + temp;
+		}else{
+			urlTemp = temp;
+		}
+
+		return  urlTemp;
 	}
 
 	//获取每页各个文章的url
